ee3fc04bac3aee98717326cc13b140c771d19873
fangyung <fangyung@mailer.blueoceanit.com.my>
2022-10-19 16:47:10 +0800

    Added Login, Register, Reset Password, Sent Email Activities

--- app/src/main/java/com/etaman/etaman/MainActivity.java
+++ app/src/main/java/com/etaman/etaman/MainActivity.java
@@ -2,13 +2,39 @@ package com.etaman.etaman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        switchRegisterActivity();
+        switchResetPasswordActivity();
+    }
+
+    private void switchRegisterActivity() {
+        TextView txtRegister = findViewById(R.id.txtRegister);
+        txtRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void switchResetPasswordActivity() {
+        TextView txtForgotPassword = findViewById(R.id.txtForgotPassword);
+        txtForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ResetPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/etaman/etaman/SentConfirmationEmailActivity.java
@@ -0,0 +1,13 @@
+package com.etaman.etaman;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SentConfirmationEmailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sent_reset_link);
+    }
+}
