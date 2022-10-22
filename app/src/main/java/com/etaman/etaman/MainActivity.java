1cecc386dfe5d0695194e60348de4e5f437a2420
fangyung <fangyung@mailer.blueoceanit.com.my>
2022-10-22 13:19:21 +0800

    Added Register and Login API

--- app/src/main/java/com/etaman/etaman/MainActivity.java
+++ app/src/main/java/com/etaman/etaman/MainActivity.java
@@ -4,16 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private String username;
+    private String password;
+    private ApiRepository api = new ApiRepository();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-         switchRegisterActivity();
-         switchResetPasswordActivity();
+        switchRegisterActivity();
+        switchResetPasswordActivity();
+        getUsername();
+        getPassword();
+        submitLogin();
     }
 
     private void switchRegisterActivity() {
@@ -34,6 +44,56 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, ResetPasswordActivity.class);
                 startActivity(intent);
+            }
+        });
+    }
+
+    private void getUsername() {
+        TextView txtUsername = findViewById(R.id.txtUsername);
+        txtUsername.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                username = txtUsername.getText().toString();
+            }
+        });
+    }
+
+    private void getPassword() {
+        TextView txtPassword = findViewById(R.id.txtPassword);
+        txtPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                password = txtPassword.getText().toString();
+            }
+        });
+    }
+
+    private void submitLogin() {
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                api.loginUserAPI(username, password);
+                if (api.userInfo != null) {
+                    Intent intent = new Intent(MainActivity.this, RequestsEventsActivity.class);
+                    startActivity(intent);
+                }
             }
         });
     }
--- app/src/main/java/com/etaman/etaman/RegisterActivity.java
+++ app/src/main/java/com/etaman/etaman/RegisterActivity.java
@@ -2,17 +2,31 @@ package com.etaman.etaman;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import java.io.IOException;
 
 public class RegisterActivity extends AppCompatActivity {
+    private String username;
+    private String password;
+    private String email;
+    private ApiRepository api = new ApiRepository();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
         switchLoginActivity();
+        getUsername();
+        getPassword();
+        getEmail();
+        submitRegistration();
     }
 
     private void switchLoginActivity() {
@@ -20,6 +34,72 @@ public class RegisterActivity extends AppCompatActivity {
         txtLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void getUsername() {
+        TextView txtUsername = findViewById(R.id.txtUsername);
+        txtUsername.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                username = txtUsername.getText().toString();
+            }
+        });
+    }
+
+    private void getPassword() {
+        TextView txtPassword = findViewById(R.id.txtPassword);
+        txtPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                password = txtPassword.getText().toString();
+            }
+        });
+    }
+
+    private void getEmail() {
+        TextView txtEmail = findViewById(R.id.txtEmail);
+        txtEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                email = txtEmail.getText().toString();
+            }
+        });
+    }
+
+    private void submitRegistration() {
+        Button btnRegister = findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                api.registerUserAPI(username, email, password);
                 Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
                 startActivity(intent);
             }
